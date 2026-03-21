6d493febff9f1126bfc620b6381c5dc5b1046032
Lucas Alemão <lucasalemao23@hotmail.com>
2026-03-21 16:20:50 +0000

    Resolução de Problemas no Conversor de Moeda

--- TP1/app/src/main/java/com/example/conversor/MainActivity.kt
+++ TP1/app/src/main/java/com/example/conversor/MainActivity.kt
@@ -1,22 +1,30 @@
+package com.example.conversor
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. Ligar as variáveis (Isso precisa estar dentro do onCreate)
+        // 1. Ligar as variáveis
         val editEuro = findViewById<EditText>(R.id.editEuro)
-        val btnConvert = findViewById<Button>(R.id.btnConvert)
+        val btnConvert = findViewById<Button>(R.id.btnConverter)
         val txtResult = findViewById<TextView>(R.id.txtResultado)
 
-        // 2. Configurar o clique do botão (Também dentro do onCreate)
+        // 2. Configurar o clique do botão
         btnConvert.setOnClickListener {
             val input = editEuro.text.toString()
 
             if (input.isNotEmpty()) {
                 val euros = input.toDouble()
 
-                // Taxa fixa para o exemplo (ou você pode usar a API que discutimos)
+                // Taxa fixa para o exemplo
                 val taxaDolar = 1.10
                 val resultado = euros * taxaDolar
 
